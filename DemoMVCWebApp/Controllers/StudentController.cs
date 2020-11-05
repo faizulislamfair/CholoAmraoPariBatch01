b4029044b9a1ec81f25e3c942dfa0dd39b2db94b
Abu Sayed Ahmad <sayed.ahmad@technovista.com.bd>
2020-11-05 23:25:14 +0600

    Day 2 : First commit

---
+++ DemoMVCWebApp/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using DemoMVCWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVCWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            
+            StudentModel student = new StudentModel();
+            student.Country = "Bangladesh";
+            student.Name = "Jafor";
+            student.University = "BUET";
+            student.Dept = "CSE";
+            student.Roll = 133083;
+            student.Age = 45;
+            student.District = "Dhaka";
+
+            return View(student);
+        }
+        public ActionResult CSEStudentsList()
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoMVCWebApp/Models/StudentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVCWebApp.Models
+{
+    public class StudentModel
+    {
+        public String Country { get; set; }
+        public String Name { get; set; }
+        public String University { get; set; }
+        public String Dept { get; set; }
+        public Int64 Roll { get; set; }
+        public Int64 Age { get; set; }
+        public String District { get; set; }
+    }
+}
